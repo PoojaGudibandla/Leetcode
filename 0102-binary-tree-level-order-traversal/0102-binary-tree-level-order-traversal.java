8ac722f71064d85a36c9e186bae6d02d988eafe8
Pooja Gudibandla <89799472+PoojaGudibandla@users.noreply.github.com>
2024-11-30 21:21:26 -0500

    Time: 1 ms (91.03%), Space: 45 MB (41.78%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,22 +13,47 @@
  *     }
  * }
  */
-class Solution {
-    List<List<Integer>> result;
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        result = new ArrayList<>();
-        if(root==null) return result;
-        traverse(root, 0);
-        return result;
-    }
+// class Solution {
+//     List<List<Integer>> result;
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         result = new ArrayList<>();
+//         if(root==null) return result;
+//         traverse(root, 0);
+//         return result;
+//     }
     
-    private void traverse(TreeNode node, int level){
-        if(node == null) return;
-        if(result.size()==level){
-            result.add(new ArrayList<>());
+//     private void traverse(TreeNode node, int level){
+//         if(node == null) return;
+//         if(result.size()==level){
+//             result.add(new ArrayList<>());
+//         }
+//         result.get(level).add(node.val);
+//         traverse(node.left, level+1);
+//         traverse(node.right, level+1);
+//     }
+// }
+
+class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root){
+    List<List<Integer>> result = new ArrayList<>();
+    if(root==null) return result;
+    Queue<TreeNode> queue= new LinkedList<>();
+    queue.offer(root);
+    while(!queue.isEmpty()){
+      int levelSize = queue.size();
+      List<Integer> currentLevel = new ArrayList<>();
+      for( int i=0; i<levelSize;i++){
+        TreeNode current= queue.poll();
+        currentLevel.add(current.val);
+        if(current.left !=null){
+          queue.offer(current.left);
         }
-        result.get(level).add(node.val);
-        traverse(node.left, level+1);
-        traverse(node.right, level+1);
+        if(current.right !=null){
+          queue.offer(current.right);
+        }
+      }
+      result.add(currentLevel);
     }
+    return result;
+}
 }
