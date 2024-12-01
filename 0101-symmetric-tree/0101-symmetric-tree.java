3da66b48c39ebbf95ce6fa11b7e2176acb1caa0e
Pooja Gudibandla <89799472+PoojaGudibandla@users.noreply.github.com>
2024-11-30 22:00:31 -0500

    Time: 0 ms (100.00%), Space: 41.8 MB (33.25%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,14 +13,27 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return root==null || isSymmetric(root.left, root.right);
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return root==null || isSymmetric(root.left, root.right);
+//     }
+//     public boolean isSymmetric(TreeNode left, TreeNode right){
+//         if(left==null ||right==null)
+//           return left==right;
+//         if(left.val !=right.val) return false;
+//         return isSymmetric(left.left, right.right) &&isSymmetric(left.right, right.left);
+//     }
+// }
+class Solution{
+     public boolean isSymmetric(TreeNode root) {
+         return isMirror(root, root);
+     }
+    public boolean isMirror(TreeNode tree1, TreeNode tree2){
+        if(tree1 == null && tree2==null)
+            return true;
+        if(tree1 == null || tree2==null)
+            return false;
+    return ((tree1.val ==tree2.val)&&isMirror(tree1.left, tree2.right)&&isMirror(tree1.right, tree2.left));
     }
-    public boolean isSymmetric(TreeNode left, TreeNode right){
-        if(left==null ||right==null)
-          return left==right;
-        if(left.val !=right.val) return false;
-        return isSymmetric(left.left, right.right) &&isSymmetric(left.right, right.left);
-    }
+    
 }
